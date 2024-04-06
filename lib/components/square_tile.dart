3d94e92b7826626a2840b5f2f7f8e1778eb3bea1
ilmamhf <ilmamhf643@gmail.com>
2024-04-06 10:55:15 +0700

    login page ui

---
+++ lib/components/square_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SquareTile extends StatelessWidget {
+  const SquareTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_projek_app/components/my_button.dart';
 import 'package:flutter_projek_app/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,6 +8,9 @@ class LoginPage extends StatelessWidget {
   // text editing controllers
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
+
+  // sign user method
+  void signUserIn() {}
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +52,7 @@ class LoginPage extends StatelessWidget {
           
               // forgot password?
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
@@ -58,13 +62,83 @@ class LoginPage extends StatelessWidget {
                     ),
                   ],
                 ),
-              )
+              ),
+
+              const SizedBox(height: 20),
           
               // sign in button
+              MyButton(
+                onTap: signUserIn,
+              ),
+
+              const SizedBox(height: 30),
+
+              // or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
           
               // google sign in button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // google button
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: Colors.grey[400],
+                      ),
+                    child: Text(
+                      "G", 
+                      style: TextStyle(fontSize: 32),
+                    )
+                  )
+                ],
+              ),
+
+              SizedBox(height: 20,),
           
               // register
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Not a member?"),
+                  const SizedBox(width: 4,),
+                  Text(
+                    "Register now",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+
             ],
           ),
         ),
